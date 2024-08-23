9183eeea1e3f7ca970350efee5ec3d1a970862f5
b14ckw1d0w <55816111+b14ckw1d0w@users.noreply.github.com>
2024-08-23 16:00:32 +0100

    Update install.sh

--- main/install.sh
+++ main/install.sh
@@ -18,9 +18,9 @@
 # #      GitHub: www.github.com/bl4ckw1d0w/dracut-network-manager-wireless
 # #      E-mail: bl4ckw1d0w.github@gmail.com
 
-cd ~/ && mkdir dracut-network-manager-wireless-v1.0
-curl -fsSl "https://raw.githubusercontent.com/b14ckw1d0w/dracut-network-manager-wireless/main/main/dracut-network-manager-wireless-v1.0.zip" -o /tmp/dracut-network-manager-wireless-v1.0.zip
-unzip dracut-network-manager-wireless-v1.0.zip -d /tmp/dracut-network-manager-wireless-v1.0
+cd ~/ && mkdir dracut-network-manager-wireless-v1.0                                            ─╯
+curl -fsSl "https://raw.githubusercontent.com/b14ckw1d0w/dracut-network-manager-wireless/main/main/dracut-network-manager-wireless-v1.0.zip" -o ~/dracut-network-manager-wireless-v1.0.zip
+unzip dracut-network-manager-wireless-v1.0.zip -d ~/dracut-network-manager-wireless-v1.0
 cd dracut-network-manager-wireless-v1.0
 rm -rf /etc/dracut.conf.d/dracut-network-manager-wireless /usr/lib/dracut/modules.d/30network-manager-wireless/
 rsync -av --inplace "./" /
